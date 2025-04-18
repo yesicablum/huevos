bc2d3dac002fc68fd7513ff10cf0989e8491c324
yesicablum <yesica.blum@uniminuto.edu.co>
2025-04-17 21:24:23 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,4 @@
+function Home() {
+    return <h1>Página de Inicio</h1>
+}
+export default Home
